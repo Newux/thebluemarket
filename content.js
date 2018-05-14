e967c3dcaad1f31e914e82e43f3828acbdf2d5f1
Newux <39223283+Newux@users.noreply.github.com>
2018-05-14 11:31:48 -0400

    Update content.js

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-marketplace_slogan = "EMPOWERING LOCAL SELLERS.";
-marketplace_description = "HELPING YOU FIND THE BEST DEALS.";
+marketplace_slogan = "HELPING YOU FIND THE BEST DEALS.";
+marketplace_description = "SAVE UP TO 90% OFF ON RETAIL PRICES.";
 display_red_banner = true;
 
