8b62f33d3da461c36f80d40799ad9cfe67151c33
Newux <39223283+Newux@users.noreply.github.com>
2018-05-14 21:28:19 +0545

    Update custom.js

--- custom.js
+++ custom.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     addBannerForNotLoggedIn();
     addFooter();
     addRedBanner();
-    updateLogo();
+    //updateLogo();
     updateGridView();
     customizePeopleGrid();
     customizeListView();
