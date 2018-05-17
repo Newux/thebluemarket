31b8065d2d5afd348c17c39bd8c836680c293cc0
Newux <39223283+Newux@users.noreply.github.com>
2018-05-17 18:13:13 -0400

    Update content.js

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
 marketplace_slogan = "HELPING YOU FIND THE BEST DEALS.";
 marketplace_description = "SAVE UP TO 90% OFF ON RETAIL PRICES.";
-display_red_banner = true;
+display_red_banner = false;
 
