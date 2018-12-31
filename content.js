f7a0696c95a31951fb42f91d643c9c893da55e86
Newux <39223283+Newux@users.noreply.github.com>
2018-12-31 13:40:20 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
 marketplace_slogan = "HELPING YOU FIND THE BEST DEALS.";
-marketplace_description = "Online & Indoor / Outdoor Patio Sale ----------------------------- Last Saturday in every month 1pm - 3pm -----------------------------";
+marketplace_description = "---------- Online & Indoor / Outdoor Patio Sale ---------- Last Saturday in every month 1pm - 3pm ----------";
 display_red_banner = false;
 
