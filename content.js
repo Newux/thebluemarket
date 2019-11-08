5d99b92f36af469cd9cf2d6e7391725ad8987c1e
Newux <39223283+Newux@users.noreply.github.com>
2019-11-07 22:47:50 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
 marketplace_slogan = "PLANNING AN EVENT?";
-marketplace_description = "CHECK OUT OUR LISTING OF LOCAL VENDORS BELOW";
+marketplace_description = "CHECK OUT OUR LISTING OF LOCAL VENDORS";
 display_red_banner = false;
 
